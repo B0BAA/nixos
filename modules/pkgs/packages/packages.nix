2dd344bc12e16b5be7ba009b67f8e50902fa5eac
B0BAA <saatu2005@gmail.com>
2020-11-14 13:39:01 +0530

    added package

--- modules/pkgs/packages/packages.nix
+++ modules/pkgs/packages/packages.nix
@@ -3,7 +3,7 @@
 {
   environment.systemPackages = with pkgs; [ #all system-wide packages
      wget vim neovim feh polybar libnotify st busybox pciutils mkinitcpio-nfs-utils pass 
-     firefox-bin xorg.xkill nitrogen killall zip toybox
+     firefox-bin xorg.xkill nitrogen killall zip toybox gnupg
      fish multimc alacritty git dmenu w3m xmobar pavucontrol youtube-dl zsh zsh-syntax-highlighting zsh-fast-syntax-highlighting
      ranger neofetch discord htop fzf xclip obs-studio
      wireshark spotify unzip wireguard teams oraclejdk8 xclip
